5f8997c8b72d89d95f31b7f53cb46afcb03e2d27
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-03-12 00:31:19 +0200

    healthcheck logs

--- apps/detective/votes/consumer/votes_consumer.rs
+++ apps/detective/votes/consumer/votes_consumer.rs
@@ -61,10 +61,14 @@ async fn main() -> Result<()> {
 
     let work_queue = WorkQueue::new(KeyPrefix::from("votes"));
 
-    let app = Router::new().route("/", axum::routing::get(|| async { "ok" }));
-    let listener = tokio::net::TcpListener::bind("localhost:3000")
-        .await
-        .unwrap();
+    let app = Router::new().route(
+        "/",
+        axum::routing::get(|| async {
+            info!("healthcheck");
+            "ok"
+        }),
+    );
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 
     loop {
