ec824e9f25f20fbea9a0d037b6a6cadfff4287d5
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-03-15 22:42:09 +0200

    clean

--- libs/ts/db/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Kysely } from "kysely";
-import type { DB } from "./kysely";
-declare const db: Kysely<DB>;
-export default db;
-export * from "./kysely";
-export { jsonArrayFrom } from "kysely/helpers/mysql";
-//# sourceMappingURL=index.d.ts.map
