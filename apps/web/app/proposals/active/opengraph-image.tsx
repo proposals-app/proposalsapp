5fe1ef601d5ec9f54a49f5b22265f208f107e1e8
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-03-23 13:01:04 +0200

    Update opengraph-image.tsx

--- apps/web/app/proposals/active/opengraph-image.tsx
+++ apps/web/app/proposals/active/opengraph-image.tsx
@@ -5,21 +5,40 @@ export default function Image({
 }: {
   searchParams: { from: string; voted: string; proxy: string };
 }) {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 128,
-          background: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        ${searchParams.from} - ${searchParams.voted} - ${searchParams.proxy}
-      </div>
-    ),
-  );
+  if (searchParams && searchParams.from)
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            fontSize: 128,
+            background: "white",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          ${searchParams.from}
+        </div>
+      ),
+    );
+  else
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            fontSize: 128,
+            background: "white",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          All daos???
+        </div>
+      ),
+    );
 }
