5bf7818f7394e3fd5ca599ada9e12a9b16cf2bc0
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-07-05 10:44:11 +0300

    Update header.tsx

--- libs/ts/emails/components/header.tsx
+++ libs/ts/emails/components/header.tsx
@@ -4,24 +4,18 @@ import { baseUrl } from "../src/const";
 
 const Header = () => (
   <Section className="w-full py-4">
-    <Section
-      className="hidden w-fit rounded-lg bg-[#2C2927] px-4 py-3 lg:block"
-      align="left"
-    >
+    <Section className="hidden w-fit rounded-lg lg:block" align="left">
       <Img
-        src={`${baseUrl}/assets/icons/web/logo-lettering.png`}
-        width="200"
+        src={`${baseUrl}/assets/email/logo-lettering.png`}
+        width="250"
         alt="proposals.app"
       />
     </Section>
 
-    <Section
-      className="block w-fit rounded-lg bg-[#2C2927] px-4 py-3 lg:hidden"
-      align="center"
-    >
+    <Section className="block w-fit rounded-lg lg:hidden" align="center">
       <Img
-        src={`${baseUrl}/assets/icons/web/logo-lettering.png`}
-        width="200"
+        src={`${baseUrl}/assets/email/logo-lettering.png`}
+        width="250"
         alt="proposals.app"
       />
     </Section>
