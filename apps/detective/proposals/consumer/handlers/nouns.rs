6642b2cdc57cd38f53f87669ae6824df459afe47
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-09-10 20:26:46 +0300

    Update nouns.rs

--- apps/detective/proposals/consumer/handlers/nouns.rs
+++ apps/detective/proposals/consumer/handlers/nouns.rs
@@ -190,7 +190,8 @@ async fn data_for_proposal(
 
     let quorum = onchain_proposal.quorum_votes.as_u128() as f64;
 
-    let scores_quorum = onchain_proposal.for_votes.as_u128() as f64;
+    let scores_quorum = onchain_proposal.against_votes.as_u128() as f64
+        + onchain_proposal.abstain_votes.as_u128() as f64;
 
     let proposal_state = gov_contract
         .state(log.id)
@@ -237,6 +238,6 @@ async fn data_for_proposal(
         votes_index: NotSet,
         votes_fetched: NotSet,
         votes_refresh_speed: NotSet,
-        metadata: NotSet
+        metadata: NotSet,
     })
 }
