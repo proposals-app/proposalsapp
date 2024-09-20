d728a66c435972218e7bb6bf43aabbb91f5b15f6
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-09-20 16:10:26 +0300

    Update posts.rs

--- apps/discourse/fetchers/posts.rs
+++ apps/discourse/fetchers/posts.rs
@@ -56,6 +56,7 @@ impl PostFetcher {
 
             if response.posts_count <= current_posts_count as i32 {
                 info!("No new posts to fetch for topic {}. Stopping.", topic_id);
+                sleep(Duration::from_secs_f32(0.5)).await;
                 break;
             }
 
