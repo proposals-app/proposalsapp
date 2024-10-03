af69edab8fddf1686df5fb45c92044294289c563
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-10-03 21:29:22 +0300

    deps

--- apps/discourse/fetchers/topics.rs
+++ apps/discourse/fetchers/topics.rs
@@ -30,7 +30,7 @@ impl TopicFetcher {
 
         loop {
             let url = format!(
-                "{}/latest.json?order=created&ascending=false&page={}",
+                "{}/latest.json?order=created&ascending=true&page={}",
                 self.base_url, page
             );
             let response: TopicResponse = self.api_handler.fetch(&url).await?;
