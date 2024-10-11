71417485a383b956e427cf7b2defa3ed8fd2e83e
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-10-11 14:05:56 +0300

    Update aave_v2_mainnet_proposals.rs

--- apps/detective/indexers/aave_v2_mainnet_proposals.rs
+++ apps/detective/indexers/aave_v2_mainnet_proposals.rs
@@ -448,13 +448,6 @@ mod aave_v2_content {
         );
 
         assert_eq!(
-            get_title("12f2d9c91e4e23ae4009ab9ef5862ee0ae79498937b66252213221f04a5d5b32".into())
-                .await
-                .unwrap(),
-            "Add 1INCH to Aave v2 market"
-        );
-
-        assert_eq!(
             get_title("e7e93497d3847536f07fe8dba53485cf68a275c7b07ca38b53d2cc2d43fab3b0".into())
                 .await
                 .unwrap(),
@@ -485,15 +478,6 @@ mod aave_v2_content {
             .await
             .unwrap(),
             "https://governance.aave.com/t/arfc-polygon-v3-supply-cap-update-2023-05-21/13161"
-        );
-
-        assert_eq!(
-            get_discussion(
-                "12f2d9c91e4e23ae4009ab9ef5862ee0ae79498937b66252213221f04a5d5b32".into()
-            )
-            .await
-            .unwrap(),
-            "https://governance.aave.com/t/arc-add-1inch-as-collateral/8056"
         );
 
         assert_eq!(
@@ -529,13 +513,6 @@ mod aave_v2_content {
                 .await
                 .unwrap(),
             "---\ntitle: Polygon Supply Cap Update\ndiscussions: https://governance.aave.com/t/arfc-polygon-v3-supply-cap-update-2023-05-21/13161\nshortDescription: Increase SupplyCap stMATIC and MaticX on Polygon v3 from 30M units to 40M units and 29.3M to 38M units respectively.\nauthor: Llama (Fermin Carranza and TokenLogic)\ncreated: 2023-06-08\n---\n\n# Summary\n\nThis AIP increases the stMATIC and MaticX Supply Cap on Polygon to 40M units and 38M units respectively.\n\n# Abstract\n\nThe utilisation of the stMATIC and MaticX Supply Caps has reached 100% and is preventing strategies built on Aave Protocol from depositing into these reserves.\n\nThe yield maximising strategy is current incentivised by two communities, Polygon Foundation and Stader Labs. These incentives encourage users to deposit either Liquid Staking Token (LST) into Aave v3 Protocl.\n\nThis AIP, with support from Chaos Labs and a favourable Snapshot, shall increase the Supply Caps to enable users to deposit funds in the stMATIC and MaticX reserves on Polygon v3.\n\n# Motivation\n\nOver the previous months, Llama has been working with various communities to craft favourable conditions on Aave v3 Polygon to facilitate the creation of several yield aggregation products. These products are now active with more soon to be deployed.\n\nWith conservative Supply Caps being implemented and filled within minutes, or hours, communities who have built products on Aave v3 are experiencing great frustration. After investing time, resources and incurring audit costs, these communities are unable to promote their products to prospective users without the newly implemented Supply Caps being filled.\n\nFor these integrations to be successful, the Supply Caps need to be increased such that a wide array of prospective users can enter into these automated strategies. Currently, the smaller Supply Cap increases are filling quickly and we are experiencing several whales / products absoring near 100% of newly implemented Supply Caps is not promoting a desirable UX for users.\n\nThe original ARFC links below:\n\n- [MaticX Forum Post](https://governance.aave.com/t/arfc-polygon-supply-cap-update-23-05-2023/13190)\n- [stMATIC Forum Post](https://governance.aave.com/t/arfc-polygon-v3-supply-cap-update-2023-05-21/13161)\n\nThe proposed Supply Caps are supported by Chaos Labs. However, Gauntlet has a more conservative risk model which limits the Supply Cap to 50% of supply on the network relative to Chaose Lab's 75%. This difference leads to differing Supply Cap recommendations. Do note, Aave DAO voted to increase the Supply Caps to [75%](https://snapshot.org/#/aave.eth/proposal/0xf9261916c696ce2d793af41b7fe556896ed1ff7a8330b7d0489d5567ebefe3ba) of supply. A Snapshot vote for each Supply Cap is linked below:\n\n- [MaticX Snapshot](https://snapshot.org/#/aave.eth/proposal/0xbbb92805d7b15d46d668cdc8e40d9a15e6a3ed2ac94802667e7d3c35a763bc8c)\n- [stMATIC Snapshop](https://snapshot.org/#/aave.eth/proposal/0xd0e157ef44b5429df7e412126d632afa1192f84fa6045dcdcaed61bc79ad1b45)\n\nThe community has shown clear support for increasing the Supply Caps in line with the original.\n\nThe stMATIC and MaticX Supply Cap on Polygon to 40M units and 38M units respectively.\n\n# Specification\n\nThe following risk parameters changes are presented:\n\n**Polygon**\n\nTicker: stMATIC\n\nContract: [`0x3a58a54c066fdc0f2d55fc9c89f0415c92ebf3c4`](https://polygonscan.com/address/0x3a58a54c066fdc0f2d55fc9c89f0415c92ebf3c4)\n\n| Parameter | Current Value    | Proposed Value   |\n| --------- | ---------------- | ---------------- |\n| SupplyCap | 32,000,000 units | 40,000,000 units |\n\nTicker: MaticX\n\nContract: [`0xfa68FB4628DFF1028CFEc22b4162FCcd0d45efb6`](https://polygonscan.com/address/0xfa68fb4628dff1028cfec22b4162fccd0d45efb6)\n\n| Parameter | Current Value    | Proposed Value   |\n| --------- | ---------------- | ---------------- |\n| SupplyCap | 29,300,000 units | 38,000,000 units |\n\n# Implementation\n\nA list of relevant links like for this proposal:\n\n- [Test Cases](https://github.com/bgd-labs/aave-proposals/blob/main/src/AaveV3CapsUpdates_20230610/AaveV3PolCapsUpdates_20230610_PayloadTest.t.sol)\n- [Payload Implementation](https://github.com/bgd-labs/aave-proposals/blob/main/src/AaveV3CapsUpdates_20230610/AaveV3PolCapsUpdates_20230610_Payload.sol)\n\nThe proposal Payload was reviewed by [Bored Ghost Developing](https://bgdlabs.com/).\n\n# Copyright\n\nCopyright and related rights waived via [CC0](https://creativecommons.org/publicdomain/zero/1.0/).\n"
-        );
-
-        assert_eq!(
-            get_body("12f2d9c91e4e23ae4009ab9ef5862ee0ae79498937b66252213221f04a5d5b32".into())
-                .await
-                .unwrap(),
-            "---\ntitle: Add 1INCH to Aave v2 market\nstatus: Proposed\nauthor: Governance House - [Llama](https://twitter.com/llama), [Matthew Graham](https://twitter.com/Matthew_Graham_), [defijesus.eth](https://twitter.com/eldefijesus)\nshortDescription: Add 1INCH as collateral on the Aave V2 market\ndiscussions: https://governance.aave.com/t/arc-add-1inch-as-collateral/8056\ncreated: 2022-07-18\n---\n\n\n## Simple Summary\n\nLlama proposes listing 1INCH, the governance token of the 1inch Network, on Aave v2 mainnet as collateral with borrowing enabled. The risk parameters detailed within have been provided by Gauntlet. \n\n## Abstract\n\n1inch is a network of decentralized protocols designed to provide the fastest and most efficient operations in the DeFi space. The 1inch Network was launched in May 2019 with the release of its Aggregation Protocol v1. The 1inch Aggregation Protocol is both the oldest and most trusted DEX aggregator in the DeFi space.\n\nListing 1INCH on Aave v2 will enable lenders to receive yield and borrowers of 1INCH to partake in 1inch’s tokenomics to vote and be reimbursed their gas cost to perform swaps on 1inch’s Aggregation Platform.\n\n## Motivation\n\nListing 1INCH on the Aave v2 market would allow Aave to benefit from the first mover advantage and recognise the efforts of the 1inch team for being supportive of Aave since before v1.  \n\nSince the original proposal to list 1INCH the team has removed minting roles on the token, introduced a progressive vesting schedule and created use cases for aTokens. The 1INCH token is supported by a Chainlink oracle feed and has favourable on-chain liquidity conditions.\n\n## Specification\n\nThis proposal initializes the 1INCH reserve, enables variable borrowing, sets a reserve factor and configures the reserve as collateral. As needed, it also connects a price source on the AaveOracle.\n\n## Test Cases\n\nAll the components involved in this proposal (tokens' implementations, interest rate strategy, oracle feed, proposal payload) have been tested and reviewed, including simulations on mainnet of the whole proposal lifecycle.\n\nLink to Test Cases: [End-to-end test suite](https://github.com/defijesus/bdg-labs-aave-v2-listings/blob/master/src/test/Validation1InchListing.sol), [aToken/stableDebtToken/variableDebtToken verification](https://github.com/defijesus/bdg-labs-aave-v2-listings/tree/master/diffs), [deployment script](https://github.com/defijesus/bdg-labs-aave-v2-listings/blob/master/scripts/1InchListingPayload.s.sol)\n\n## Implementation\n\nWe recommend the following risk parameters.\n\n### Risk Parameters:\n\nLTV 40%\n\nLiquidation Threshold 50%\n\nLiquidation Bonus 8.5%\n\nReserve Factor 20%\n\n### Variable Interest Rate Parameters:\n\nUOptimal 45%\n\nR_0 0%\n\nR_s1 7%\n\nR_s2 300%\n\n\nThe snapshot vote to add 1INCH on Aave v2 mainnet market can be found [here](https://snapshot.org/#/aave.eth/proposal/0x2ea76814a0dfcad7ea1a7b3c597f059a8d574f8143886b23043918998505f5a7).  \n\n\n- [ProposalPayload](https://etherscan.io/address/0xd417d07c20e31f6e129fa68182054b641fbec8bd#code)\n- [aINCH implementation](https://etherscan.io/address/0x130FBED7dBA2b370f0F93b0Caea2cfD9b811D66D#code)\n- [Variable Debt 1INCH implementation](https://etherscan.io/address/0x4d3707566Ee8a0ed6DE424a262050C7587da8152#code)\n- [Stable Debt 1INCH implementation](https://etherscan.io/address/0x9C2114Bf70774C36E9b8d6c790c9C14FF0d6799E#code)\n- [Interest rate strategy](https://etherscan.io/address/0xb2eD1eCE1c13455Ce9299d35D3B00358529f3Dc8#code) \n- [1INCH/ETH price feed](https://etherscan.io/address/0x72AFAECF99C9d9C8215fF44C77B94B99C28741e8#code)\n\n## Copyright\n\nCopyright and related rights waived via [CC0](https://creativecommons.org/publicdomain/zero/1.0/)."
         );
 
         assert_eq!(get_body("deadbeef".into()).await.unwrap(), "Unknown");
@@ -639,8 +616,8 @@ mod aave_v2_proposals {
             indexer_type: IndexerType::Proposals,
             portal_url: Some("placeholder".into()),
             enabled: true,
-            speed: 18686736 - 18678972,
-            index: 18678972,
+            speed: 1,
+            index: 18686736,
             dao_id: Uuid::parse_str("30a57869-933c-4d24-aadb-249557cd126a").unwrap(),
         };
 
@@ -661,27 +638,6 @@ mod aave_v2_proposals {
             Ok((proposals, _, _)) => {
                 assert!(!proposals.is_empty(), "No proposals were fetched");
                 let expected_proposals = [
-                    ExpectedProposal {
-                        index_created: 18678972,
-                        external_id: "388",
-                        name: "GHO_Incident_Report",
-                        body_contains: Some(vec!["Provide a bounty to a community member for helping to detect an incident with GHO."]),
-                        url: "https://app.aave.com/governance/proposal/?proposalId=388",
-                        discussion_url: "https://governance.aave.com/t/arfc-gho-bounty-for-integration-issue-detection/15296",
-                        choices: json!(["For", "Against"]),
-                        scores: json!([502888.9536269785, 0.0]),
-                        scores_total: 502888.9536269785,
-                        quorum: 0.0,
-                        scores_quorum: 502888.9536269785,
-                        proposal_state: ProposalState::Executed,
-                        marked_spam: None,
-                        time_created: parse_datetime("2023-11-29 18:11:35"),
-                        time_start: parse_datetime("2023-11-30 18:21:47"),
-                        time_end: parse_datetime("2023-12-03 10:49:11"),
-                        block_created: Some(18678972),
-                        txid: Some("0x013917ed90ec91ee11e34d3983b02617966c9dec7e9889e5bea74714d99317f1"),
-                        metadata: None,
-                    },
                     ExpectedProposal {
                         index_created: 18686736,
                         external_id: "389",
