81f9da6272b83f57b06c706bd966ae3976d57ca9
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-12-22 00:10:57 +0200

    Update MenuBar.tsx

--- apps/web-arbitrum/app/[dao]/[proposal_id]/components/menubar/MenuBar.tsx
+++ apps/web-arbitrum/app/[dao]/[proposal_id]/components/menubar/MenuBar.tsx
@@ -26,55 +26,59 @@ export const MenuBar = () => {
             : "mt-4"
       }`}
     >
-      <button
-        onClick={() => {
-          if (view == ViewType.FULL && !expanded) {
-            setView(ViewType.BODY);
-            setExpanded(true);
-          }
-          if (view == ViewType.FULL && expanded) {
-            setExpanded(false);
-            window.scrollTo({ top: 0, behavior: "smooth" });
-          }
-          if (view == ViewType.BODY && expanded) {
-            setView(ViewType.FULL);
-            setExpanded(false);
-            window.scrollTo({ top: 0, behavior: "smooth" });
-          }
-          if (view == ViewType.COMMENTS) {
-            setView(ViewType.BODY);
-            setExpanded(true);
-            window.scrollTo({ top: 0, behavior: "smooth" });
-          }
-        }}
-        className="flex w-full cursor-pointer items-center justify-between gap-2 rounded-full border bg-white p-2 text-sm font-bold shadow-lg transition-colors hover:bg-gray-50"
-      >
+      <button className="flex w-full items-center justify-between gap-2 rounded-full border bg-white p-2 text-sm font-bold shadow-lg transition-colors hover:bg-gray-50">
         {view == ViewType.FULL && !expanded && (
-          <>
-            <ArrowDown className="rounded-full border p-1" />
-            <div className="px-2">Read Full Proposal</div>
-          </>
+          <div
+            className="flex w-full justify-between hover:underline"
+            onClick={() => {
+              setView(ViewType.BODY);
+              setExpanded(true);
+            }}
+          >
+            <div className="flex items-center">
+              <ArrowDown className="rounded-full border p-1" />
+              <div className="px-2">Read Full Proposal</div>
+            </div>
+          </div>
         )}
         {view == ViewType.BODY && (
-          <>
+          <div
+            className="flex w-full justify-between hover:underline"
+            onClick={() => {
+              if (expanded) {
+                setView(ViewType.FULL);
+                setExpanded(false);
+                window.scrollTo({ top: 0, behavior: "smooth" });
+              }
+            }}
+          >
             <ArrowUp className="rounded-full border p-1" />
             <div className="flex flex-row items-center gap-2">
               <div className="px-2">Comments and Votes</div>
               <ArrowDown className="rounded-full border p-1" />
             </div>
-          </>
+          </div>
         )}
         {view == ViewType.COMMENTS && (
-          <>
+          <div className="flex w-full justify-between">
             <div className="flex flex-row items-center gap-2">
               <ArrowUp className="rounded-full border p-1" />
-              <div className="px-2">Read Full Proposal</div>
+              <div
+                className="px-2 hover:underline"
+                onClick={() => {
+                  setView(ViewType.BODY);
+                  setExpanded(true);
+                  window.scrollTo({ top: 0, behavior: "smooth" });
+                }}
+              >
+                Read Full Proposal
+              </div>
             </div>
             <div className="flex gap-2">
               <div>filter</div>
               <div>filter</div>
             </div>
-          </>
+          </div>
         )}
       </button>
     </div>
