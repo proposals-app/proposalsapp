ada715a2ea195f7d8ab0320e29ca8fdc8ac7d955
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-12-22 21:43:17 +0200

    wip

--- apps/web-arbitrum/app/[dao]/[proposal_id]/components/menubar/CommentsViewBar.tsx
+++ apps/web-arbitrum/app/[dao]/[proposal_id]/components/menubar/CommentsViewBar.tsx
@@ -47,18 +47,16 @@ export const CommentsViewBar = () => {
     >
       <div className="flex w-full items-center justify-between gap-2 rounded-full border bg-white p-2 text-sm font-bold shadow-lg transition-colors hover:bg-gray-50">
         <div className="flex w-full justify-between">
-          <div className="flex flex-row items-center gap-2">
+          <div
+            className="flex flex-row items-center gap-2 hover:underline"
+            onClick={() => {
+              setView(ViewEnum.BODY);
+              setExpanded(true);
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            }}
+          >
             <ArrowUp className="rounded-full border p-1" />
-            <div
-              className="cursor-pointer px-2 hover:underline"
-              onClick={() => {
-                setView(ViewEnum.BODY);
-                setExpanded(true);
-                window.scrollTo({ top: 0, behavior: "smooth" });
-              }}
-            >
-              Read Full Proposal
-            </div>
+            <div className="cursor-pointer px-2">Read Full Proposal</div>
           </div>
           <div className="flex gap-2">
             <div className="flex items-center gap-2">
