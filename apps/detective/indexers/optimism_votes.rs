81262ec614f868c1fe25e5daed50b4f69d0c5be2
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-12-26 18:44:58 +0200

    Update optimism_votes.rs

--- apps/detective/indexers/optimism_votes.rs
+++ apps/detective/indexers/optimism_votes.rs
@@ -49,7 +49,7 @@ impl Indexer for OptimismVotesIndexer {
     }
 
     fn max_refresh_speed(&self) -> i32 {
-        100_000
+        10_000_000
     }
     fn indexer_type(&self) -> IndexerType {
         IndexerType::Votes
