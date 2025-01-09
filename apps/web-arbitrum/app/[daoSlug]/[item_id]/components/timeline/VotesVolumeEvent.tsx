daf46b250c93d476ce47dca638774c55a2293766
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-01-09 20:28:31 +0200

    Update VotesVolumeEvent.tsx

--- apps/web-arbitrum/app/[daoSlug]/[item_id]/components/timeline/VotesVolumeEvent.tsx
+++ apps/web-arbitrum/app/[daoSlug]/[item_id]/components/timeline/VotesVolumeEvent.tsx
@@ -11,7 +11,7 @@ export function VotesVolumeEvent({
 }: VotesVolumeEventProps) {
   return (
     <div
-      className={`ml-4 h-1 bg-black bg-opacity-15`}
+      className={`ml-4 h-1 bg-black bg-opacity-20`}
       style={{
         width: `${Math.max(volume * 80, 1)}%`,
       }}
