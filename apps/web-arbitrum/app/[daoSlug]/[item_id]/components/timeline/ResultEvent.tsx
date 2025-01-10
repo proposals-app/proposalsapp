23a48ea5526fda26288ce8d5e72a5e77ec8bce21
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-01-10 11:42:13 +0200

    wip

--- apps/web-arbitrum/app/[daoSlug]/[item_id]/components/timeline/ResultEvent.tsx
+++ apps/web-arbitrum/app/[daoSlug]/[item_id]/components/timeline/ResultEvent.tsx
@@ -68,9 +68,9 @@ export function ResultEvent({
   return (
     <div className="relative flex w-full items-center py-2">
       <div className="flex w-full flex-col gap-1 rounded-l-xl border bg-white px-4 py-2 pr-8">
-        <div className="absolute left-3 top-5 h-[7px] w-[7px] rounded-full border border-white bg-gray-500" />
+        <div className="absolute left-3 top-5 z-20 h-[7px] w-[7px] rounded-full bg-gray-500" />
         {!last && (
-          <div className="absolute left-3 top-[7px] z-50 h-[15px] max-h-[15px] w-0.5 translate-x-[2.5px] bg-gray-300" />
+          <div className="absolute left-3 top-[7px] z-10 h-[15px] max-h-[15px] w-0.5 translate-x-[2.5px] bg-gray-300" />
         )}
         <div className="ml-2 flex w-full items-center justify-between">
           <div className="text-xs">{content}</div>
