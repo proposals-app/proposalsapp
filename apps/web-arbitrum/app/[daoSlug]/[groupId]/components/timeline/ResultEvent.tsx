2ae00f2cfe51d098ed839821323f1052d32672a2
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-01-15 16:02:14 +0200

    Update ResultEvent.tsx

--- apps/web-arbitrum/app/[daoSlug]/[groupId]/components/timeline/ResultEvent.tsx
+++ apps/web-arbitrum/app/[daoSlug]/[groupId]/components/timeline/ResultEvent.tsx
@@ -70,30 +70,31 @@ export function ResultEvent({
   const Component = voteType ? VoteComponents[voteType] : null;
 
   return (
-    <div className="relative flex w-full items-center py-2">
-      <div className="flex w-full flex-col gap-1 rounded-l-xl border bg-white px-4 py-2 pr-8">
-        <div className="absolute left-3 top-5 z-20 h-[7px] w-[7px] rounded-full bg-gray-500" />
-        {!last && (
-          <div className="absolute left-3 top-[7px] z-10 h-[15px] max-h-[15px] w-0.5 translate-x-[2.5px] bg-gray-300" />
-        )}
-        <div className="ml-2 flex w-full items-center justify-between">
-          <div className="text-xs">{content}</div>
-          <Link
-            href={`/${daoSlug}/${groupId}/results/${resultNumber}`} // Link to the results page
-            className="text-blue-500 hover:underline"
-            prefetch={true}
-          >
-            <ArrowRight size={14} />
-          </Link>
-        </div>
-        <div className="ml-2 text-sm text-gray-600">
-          {Component ? (
-            <Component proposal={proposal} votes={votes} />
-          ) : (
-            <p>Invalid or unsupported vote type: {voteType}</p>
+    <Link
+      className="w-full"
+      href={`/${daoSlug}/${groupId}/results/${resultNumber}`} // Link to the results page
+      prefetch={true}
+    >
+      <div className="relative flex w-full items-center py-2">
+        <div className="flex w-full flex-col gap-1 rounded-l-xl border bg-white px-4 py-2 pr-8">
+          <div className="absolute left-3 top-5 z-20 h-[7px] w-[7px] rounded-full bg-gray-500" />
+          {!last && (
+            <div className="absolute left-3 top-[7px] z-10 h-[15px] max-h-[15px] w-0.5 translate-x-[2.5px] bg-gray-300" />
           )}
+          <div className="ml-2 flex w-full items-center justify-between">
+            <div className="text-xs">{content}</div>
+
+            <ArrowRight size={14} />
+          </div>
+          <div className="ml-2 text-sm text-gray-600">
+            {Component ? (
+              <Component proposal={proposal} votes={votes} />
+            ) : (
+              <p>Invalid or unsupported vote type: {voteType}</p>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
