ae89d1d0860b41c2f57a4b2ea9d70ebbbf51c241
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-01-15 16:23:31 +0200

    wip

--- apps/web-arbitrum/ecosystem.config.cjs
+++ apps/web-arbitrum/ecosystem.config.cjs
@@ -4,14 +4,14 @@ module.exports = {
       name: "web-arbitrum",
       script: "node_modules/next/dist/bin/next",
       args: "start",
-      cwd: "/app/apps/web-arbitrum",
+      cwd: "./apps/web-arbitrum", // Update the working directory
       instances: 6,
       exec_mode: "cluster",
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
       env: {
-        NODE_ENV: "production",
+        NODE_ENV: "production", // Ensure this is a standard Node.js environment value
         PROPOSALS_BIN: "web-arbitrum",
         NEXT_OTEL_VERBOSE: 1,
         PORT: 3000,
