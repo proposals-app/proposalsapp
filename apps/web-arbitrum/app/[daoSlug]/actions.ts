13bfe99029768ee19583cb2c0d24f5ac91d4bb40
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-01-17 02:38:31 +0200

    Update actions.ts

--- apps/web-arbitrum/app/[daoSlug]/actions.ts
+++ apps/web-arbitrum/app/[daoSlug]/actions.ts
@@ -1,6 +1,9 @@
 import { otel } from "@/lib/otel";
 import { AsyncReturnType } from "@/lib/utils";
 import { db } from "@proposalsapp/db";
+import { unstable_cache } from "next/cache";
+import { after } from "next/server";
+import { getGroupData } from "./[groupId]/actions";
 
 export async function getGroups(
   daoSlug: string,
@@ -98,6 +101,24 @@ export async function getGroups(
     groupsWithTimestamps.sort(
       (a, b) => b.newestItemTimestamp - a.newestItemTimestamp,
     );
+
+    // Cache each group's data
+    const cachedGetGroupData = unstable_cache(
+      async (daoSlug: string, groupId: string) => {
+        return await getGroupData(daoSlug, groupId);
+      },
+      ["group-data"],
+      { revalidate: 60 * 5, tags: ["group-data"] },
+    );
+
+    // Cache each group's data after fetching
+    after(async () => {
+      await Promise.all(
+        groupsWithTimestamps.map(async (group) => {
+          await cachedGetGroupData(daoSlug, group.id);
+        }),
+      );
+    });
 
     // Return the sorted groups without the timestamp property
     return {
