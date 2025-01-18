f3e2389bc5c510294189efd489f884a848117900
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-01-18 18:12:36 +0200

    hyperdx

--- apps/web-arbitrum/instrumentation.ts
+++ apps/web-arbitrum/instrumentation.ts
@@ -1,9 +1,8 @@
 import { registerOTel } from "@vercel/otel";
 
-const unusedVariable = 42;
-
 export function register() {
   registerOTel({
     serviceName: "arbitrum-next-app",
+    traceSampler: "always_on",
   });
 }
