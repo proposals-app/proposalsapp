f79cc49579d26ec6427e48bbb08307fa98fed6eb
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-01-22 01:33:18 +0200

    wip

--- apps/web-arbitrum/instrumentation.ts
+++ apps/web-arbitrum/instrumentation.ts
@@ -1,7 +1,5 @@
 import { registerOTel } from '@vercel/otel';
 
 export function register() {
-  registerOTel({
-    serviceName: 'arbitrum-next-app',
-  });
+  registerOTel();
 }
--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -1,7 +1,5 @@
 import { registerOTel } from "@vercel/otel";
 
 export function register() {
-  registerOTel({
-    serviceName: "next-app",
-  });
+  registerOTel();
 }
