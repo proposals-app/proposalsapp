cd3435d83433164c155f5980fa8ec7c43efc6e23
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-01-30 20:28:57 +0200

    wip

--- apps/detective/indexers/arbitrum_core_votes.rs
+++ apps/detective/indexers/arbitrum_core_votes.rs
@@ -49,7 +49,7 @@ impl Indexer for ArbitrumCoreVotesIndexer {
     }
     #[instrument(skip_all)]
     fn timeout(&self) -> Duration {
-        Duration::from_secs(60)
+        Duration::from_secs(5 * 60)
     }
 }
 
