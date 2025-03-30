ed603459b64b790df5c9aeb84259aeae98c5fb1b
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-03-30 23:57:01 +0300

    Update theme-provider.tsx

--- apps/web/app/components/theme-provider.tsx
+++ apps/web/app/components/theme-provider.tsx
@@ -4,7 +4,11 @@ import { UpdateManifest } from './update-manifest';
 
 export async function ThemeProvider({ children }: { children: ReactNode }) {
   return (
-    <NextThemesProvider attribute='class' defaultTheme='dark' enableSystem>
+    <NextThemesProvider
+      attribute='class'
+      defaultTheme='dark'
+      enableSystem={false}
+    >
       <UpdateManifest daoSlug='arbitrum' />
       <div data-theme='arbitrum' className='min-h-screen'>
         {children}
