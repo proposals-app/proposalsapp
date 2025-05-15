905b7a4a148ccb65137560ba869782f7a6adf742
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-05-15 14:51:29 +0300

    Update route.ts

--- apps/web/app/api/discourse_test/[username]/route.ts
+++ apps/web/app/api/discourse_test/[username]/route.ts
@@ -1,3 +1,25 @@
 export async function GET() {
-  return Response.json({ value: '123', url: 'https://arbitrum.proposals.app' });
+  return new Response(
+    JSON.stringify({ value: '123', url: 'https://arbitrum.proposals.app' }),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin':
+          'https://proposalapp-test.discourse.group', // or "*" for dev
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      },
+    }
+  );
+}
+
+export function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://proposalapp-test.discourse.group',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  });
 }
