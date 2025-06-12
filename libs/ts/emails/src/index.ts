54408c8b7ae1c74961af31130323199626e1549a
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-06-12 20:28:01 +0300

    Update index.ts

--- libs/ts/emails/src/index.ts
+++ libs/ts/emails/src/index.ts
@@ -4,14 +4,14 @@ export const resend = new Resend(process.env.RESEND_API_KEY);
 
 export { render } from '@react-email/render';
 
-export { default as OTPEmail } from '../emails/otp-code.tsx';
-export { default as ChangeEmailTemplate } from '../emails/change-email.tsx';
-export { default as DeleteAccountTemplate } from '../emails/delete-account.tsx';
-export { default as NewProposalEmailTemplate } from '../emails/new-proposal.tsx';
-export { default as NewDiscussionEmailTemplate } from '../emails/new-discussion.tsx';
-export { default as EndingProposalEmailTemplate } from '../emails/ending-proposal.tsx';
+export { default as OTPEmail } from '../emails/otp-code';
+export { default as ChangeEmailTemplate } from '../emails/change-email';
+export { default as DeleteAccountTemplate } from '../emails/delete-account';
+export { default as NewProposalEmailTemplate } from '../emails/new-proposal';
+export { default as NewDiscussionEmailTemplate } from '../emails/new-discussion';
+export { default as EndingProposalEmailTemplate } from '../emails/ending-proposal';
 
 // Export email template prop types
-export type { NewProposalEmailProps } from '../emails/new-proposal.tsx';
-export type { NewDiscussionEmailProps } from '../emails/new-discussion.tsx';
-export type { EndingProposalEmailProps } from '../emails/ending-proposal.tsx';
+export type { NewProposalEmailProps } from '../emails/new-proposal';
+export type { NewDiscussionEmailProps } from '../emails/new-discussion';
+export type { EndingProposalEmailProps } from '../emails/ending-proposal';
